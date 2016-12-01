a626b125dce4313ea57b30a16d69be85c8ecc8b3
MrDoni98 <danil.bartel@gmail.com>
2016-12-01 20:34:30 +0600

    Update Main.php

--- src/MrDoni98/Main.php
+++ src/MrDoni98/Main.php
@@ -7,19 +7,22 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use _64FF00\PurePerms\event\PPGroupChangedEvent;
 use pocketmine\Player;
+use pocketmine\utils\TextFormat as F;
 use pocketmine\utils\Config;
 use pocketmine\utils\Utils;
 
 class Main extends PluginBase implements Listener{
 
    public function onEnable(){
+	   if(!$this->getServer()->getPluginManager()->getPlugin("PurePerms")){
+		   $this->getLogger()->error("Установите PurePerms!");
+	   }
        @mkdir($this->getDataFolder());
        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-       $this->getLogger()->info("PPinfo is loaded");
-       $this->PP = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+       $this->getLogger()->info(F::GREEN . "PPinfo is loaded");
        $this->config = new Config($this->getDataFolder() . "config.yml" , Config::YAML, Array(
-	   'token' => '',
-	   'group' => '',
+	   'token' => 'abcd',
+	   'group' => '1234',
 	   'debug' => false));
        $this->token = $this->config->get("token");
        $this->vkGroup = $this->config->get("group");
@@ -31,14 +34,13 @@ class Main extends PluginBase implements Listener{
        $player = $event->getPlayer();
        $name = $player->getName();
        $this->getLogger()->info("Игрок ". $name." сменил группу на ". $group);
-	   $owner_id = 130963836;
        $this->wallPost("Игрок ". $name." сменил группу на ". $group);
    }
 
    public function wallPost($text) {
 	    if($this->debug === true){
 			$request = Utils::getURL("https://api.vk.com/method/wall.post?&owner_id=-".$this->vkGroup."&from_group=1&message=" .urlencode($text)."&access_token=".$this->token);
-		    $this->getLogger()->info("Результат :" . var_dump(json_decode($request)));
+		    $this->getLogger()->info(F::YELLOW."Результат :" . var_dump(json_decode($request)));
 		}
         Utils::getURL("https://api.vk.com/method/wall.post?&owner_id=-".$this->vkGroup."&from_group=1&message=" .urlencode($text)."&access_token=".$this->token);
 	}
